abd4b728ce2e5a1e0e112daccb9880effc9356ce
MilesNeitz <MilesNeitz@gmail.com>
2016-11-16 15:47:28 -0600

    changed search for authors tweets to search for user

--- src/app/mainpage/search/search.component.js
+++ src/app/mainpage/search/search.component.js
@@ -2,12 +2,15 @@ import templateUrl from './search.component.html'
 
 /* @ngInject */
 class SearchController {
-  constructor ($log, $searchService) {
+  constructor ($log, $searchService, $profileService) {
     this.$searchService = $searchService
+    this.$profileService = $profileService
     $log.debug('SearchController instantiated')
   }
+  userProfile (username) {
+    this.$profileService.viewProfile(username)
+  }
 }
-
 export const search = {
   templateUrl,
   controller: SearchController,
