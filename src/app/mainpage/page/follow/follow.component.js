5ad1ed8d8a7057a21639e583632907cb8600b880
Yer <yvyang13@gmail.com>
2016-11-16 11:05:49 -0600

    got the tweet to display

--- src/app/mainpage/page/follow/follow.component.js
+++ src/app/mainpage/page/follow/follow.component.js
@@ -2,13 +2,13 @@ import templateUrl from './follow.component.html'
 
 /* @ngInject */
 class followController {
-  constructor ($log, $followservice, $authenticate) {
+  constructor ($log, $state, $followservice, $authenticate, $tweetservice) {
     this.$followservice = $followservice
     this.$authenticate = $authenticate
+    this.$state = $state
+    this.$tweetservice = $tweetservice
     this.initiatefollowers = $followservice.getfollower($authenticate.$cookies.get('username'))
     this.initiatefollowings = $followservice.getfollowing($authenticate.$cookies.get('username'))
-    console.log($followservice.number)
-    console.log($authenticate.$cookies.get('username'))
     $log.debug('FollowController instantiated')
   }
 
@@ -19,6 +19,14 @@ class followController {
   getfollowing () {
     return this.$followservice.arrfollowing
   }
+
+  google (username) {
+    console.log(username)
+    this.$tweetservice.setusername(username)
+    this.$tweetservice.gettweets()
+    this.$state.go('mainpage.page.tweet')
+  }
+
 }
 
 export const follow = {
