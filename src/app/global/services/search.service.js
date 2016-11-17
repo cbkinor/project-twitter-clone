1681ba64de918bf4571ce9215e6c8b1b07f77aa6
MilesNeitz <MilesNeitz@gmail.com>
2016-11-17 10:32:10 -0600

    added like and unlike for tweets

--- src/app/global/services/search.service.js
+++ src/app/global/services/search.service.js
@@ -1,15 +1,75 @@
 export class SearchService {
 
   /* @ngInject */
-  constructor ($log, $http, $state, $cookies) {
+  constructor ($log, $http, $state, $cookies, $authenticate) {
     this.$log = $log
     this.$http = $http
     this.$state = $state
     this.$cookies = $cookies
+    this.$authenticate = $authenticate
     this.searchText = undefined
     $log.debug('SearchService created')
   }
+  likeTweet (item) {
+    this.$http({
+      method: 'POST',
+      url: 'http://localhost:8080/tweets/' + item.id + '/like',
+      data: {
+        username: this.$authenticate.username,
+        password: this.$authenticate.password
+      }
+    }).then(
+          (response) => {
+            this.$log.debug(response)
+            item.liked = true
+          },
+          (error) => {
+            this.$log.debug(error)
+          }
+        )
+  }
+  unlikeTweet (item) {
+    this.$http({
+      method: 'POST',
+      url: 'http://localhost:8080/tweets/' + item.id + '/unlike',
+      data: {
+        username: this.$authenticate.username,
+        password: this.$authenticate.password
+      }
+    }).then(
+          (response) => {
+            this.$log.debug(response)
+            item.liked = false
+          },
+          (error) => {
+            this.$log.debug(error)
+          }
+        )
+  }
 
+  checkLikeTweet (item) {
+    this.$http({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets/' + item.id + '/likes',
+      data: {
+        username: this.$authenticate.username,
+        password: this.$authenticate.password
+      }
+    }).then(
+          (response) => {
+            this.$log.debug(response)
+            response.data.filter(function (user) { return user.username === this.$authenticate.username })
+            if (response.data.length() > 0) {
+              item.liked = true
+            } else {
+              item.liked = false
+            }
+          },
+          (error) => {
+            this.$log.debug(error)
+          }
+        )
+  }
   search () {
     this.mentioned = undefined
     this.author = undefined
